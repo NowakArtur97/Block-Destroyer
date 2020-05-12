b02060dcd11084d58d69b45f1b88911b2a0bf373
NowakArtur97 <nowart97@gmail.com>
2020-05-12 20:18:26 +0200

    Push Ball on Mouse Click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,8 +6,13 @@ public class Ball : MonoBehaviour
 {
     [SerializeField]
     private Paddle mainPaddle;
+    [SerializeField]
+    private float xPush = 0f;
+    [SerializeField]
+    private float yPush = 15f;
 
     private Vector2 ballStartingPos;
+    private bool gameHasStarted = false;
 
     private void Start()
     {
@@ -16,7 +21,27 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
+        if (!gameHasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+    }
+
+    private void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameHasStarted = true;
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(mainPaddle.transform.position.x, ballStartingPos.y);
+
         transform.position = paddlePos;
     }
 }
