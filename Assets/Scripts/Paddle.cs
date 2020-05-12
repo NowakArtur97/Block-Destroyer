31373d7ea295e5d96273ecd0076036070fa687fc
NowakArtur97 <nowart97@gmail.com>
2020-05-12 19:51:53 +0200

    Make Paddle move after mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    private float paddleYPosition = 1f;
+    [SerializeField]
+    private float minX = 1.3f;
+    [SerializeField]
+    private float maxX= 20f;
+
+    private void Update()
+    {
+        Vector3 mousePosInWorldUnits = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 paddlePos = new Vector2(mousePosInWorldUnits.x, paddleYPosition);
+
+        paddlePos.x = Mathf.Clamp(paddlePos.x, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
