6311d819a3785e36720b05d413defa217c1a5e93
NowakArtur97 <nowart97@gmail.com>
2020-05-13 18:50:15 +0200

    Make Level Manager a Singleton and create walls to block ball movements and change gravity

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,19 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
+        int numberOfLevelManagers = FindObjectsOfType<LevelManager>().Length;
+
+        if (numberOfLevelManagers > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
