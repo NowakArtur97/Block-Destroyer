fd451ae04df09204741f305ae1b558e72527c157
NowakArtur97 <nowart97@gmail.com>
2020-05-13 20:14:32 +0200

    Add music to level and make Sound Player a singleton

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private void Start()
+    {
+        int numberOfMusicPlayers = FindObjectsOfType<Music>().Length;
+
+        if (numberOfMusicPlayers > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
