7c453ba548e7dd9c9e784b32a371d5a338af945f
NowakArtur97 <nowart97@gmail.com>
2020-05-13 19:21:06 +0200

    Play sounds on Ball Collision

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,13 +8,19 @@ public class Ball : MonoBehaviour
     private float xPush = 0f;
     [SerializeField]
     private float yPush = 15f;
+    [SerializeField]
+    private AudioClip[] sounds;
 
     private Vector2 ballStartingPos;
     private bool gameHasStarted = false;
 
+    private AudioSource audioSource;
+
     private void Start()
     {
         ballStartingPos = new Vector2(transform.position.x, transform.position.y);
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -41,5 +47,19 @@ public class Ball : MonoBehaviour
         Vector2 paddlePos = new Vector2(mainPaddle.transform.position.x, ballStartingPos.y);
 
         transform.position = paddlePos;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameHasStarted)
+        {
+            PlayRandomSound();
+        }
+    }
+
+    private void PlayRandomSound()
+    {
+        AudioClip randomSound = sounds[Random.Range(0, sounds.Length)];
+        audioSource.PlayOneShot(randomSound);
     }
 }
