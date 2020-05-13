c57b564d9de5749d6bb25078a5e5f658afcbaff5
NowakArtur97 <nowart97@gmail.com>
2020-05-13 18:33:11 +0200

    Add physics material to Ball and create Lose Collider and Scene Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+
+    public void RestartLevel()
+    {
+        Debug.Log("Restart Level");
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
