47b833aa751068006daf718b643120fe17acb082
NowakArtur97 <nowart97@gmail.com>
2020-05-15 19:01:23 +0200

    Destroy Blocks and make them Crack on Collision

---
+++ Assets/Scripts/Crack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Crack : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] hitSprites;
+    [SerializeField]
+    private float damageDealtOnCollision = 1f;
+
+    private Health health;
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health.DealDamage(damageDealtOnCollision);
+
+        int healtLeft = (int)health.GetHealth() - 1;
+
+        if (!health.IsDead())
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[healtLeft];
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private float health = 4;
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    private void Update()
+    {
+        if (IsDead())
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsDead()
+    {
+        return health <= 0;
+    }
+}
