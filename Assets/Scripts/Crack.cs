17ce284288fb0e7bcd2982adf6823515cc28c838
NowakArtur97 <nowart97@gmail.com>
2020-05-15 20:16:39 +0200

    Create Particle effect on collision between Ball and Block

--- Assets/Scripts/Crack.cs
+++ Assets/Scripts/Crack.cs
@@ -8,21 +8,28 @@ public class Crack : MonoBehaviour
     private float damageDealtOnCollision = 1f;
 
     private Health health;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         health = GetComponent<Health>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         health.DealDamage(damageDealtOnCollision);
 
+        CrackObject();
+    }
+
+    private void CrackObject()
+    {
         int healtLeft = (int)health.GetHealth() - 1;
 
         if (!health.IsDead())
         {
-            GetComponent<SpriteRenderer>().sprite = hitSprites[healtLeft];
+            spriteRenderer.sprite = hitSprites[healtLeft];
         }
     }
 }
---
+++ Assets/Scripts/Glint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glint : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject glintVFX;
+    [SerializeField]
+    private float durationOfGlint = 1f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GlintObject();
+    }
+
+    private void GlintObject()
+    {
+        GameObject glint = Instantiate(glintVFX, transform.position, transform.rotation);
+        Destroy(glint, durationOfGlint);
+    }
+}
