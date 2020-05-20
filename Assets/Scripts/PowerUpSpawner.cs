31602fccd608611aebf04ee8bc9d92c522be9d9c
NowakArtur97 <nowart97@gmail.com>
2020-05-20 18:25:09 +0200

    Create Power Up Spawner and prepare for spawning

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float minSpawnDelay = 5f;
+    [SerializeField]
+    private float maxSpawnDelay = 10f;
+
+    private bool isSpawning = false;
+
+    private GameSession gameSession;
+
+    private void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    private void Update()
+    {
+        if (gameSession.HasGameStarted() && !isSpawning)
+        {
+            StartCoroutine(SpawningCoroutine());
+        }
+    }
+
+    private IEnumerator SpawningCoroutine()
+    {
+        isSpawning = true;
+
+        yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+
+        isSpawning = false;
+
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        Debug.Log("Time to spawn");
+    }
+}
