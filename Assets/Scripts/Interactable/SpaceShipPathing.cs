e848ce6d48eba183689015a66cb84e7a970a2261
NowakArtur97 <nowart97@gmail.com>
2020-07-06 18:01:36 +0200

    Create Space Ship Pathing and waypoints for the first Space Ship

---
+++ Assets/Scripts/Interactable/SpaceShipPathing.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipPathing : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> waypoints;
+    [SerializeField]
+    private float speed = 10f;
+
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        MoveTo();
+    }
+
+    private void MoveTo()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+
+            transform.position = waypoints[waypointIndex].transform.position;
+        }
+    }
+}
