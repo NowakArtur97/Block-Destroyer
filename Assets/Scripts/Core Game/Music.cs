7a5105f6ba37c2754bd244e77e93cccbbe37cfcb
NowakArtur97 <nowart97@gmail.com>
2020-07-13 19:51:49 +0200

    Add soundtrack for each Level

--- Assets/Scripts/Core Game/Music.cs
+++ Assets/Scripts/Core Game/Music.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip[] soundTrack;
+
+    private AudioSource audioSource;
+    private AudioClip song;
+
     private void Start()
     {
         int numberOfMusicPlayers = FindObjectsOfType<Music>().Length;
@@ -14,6 +21,17 @@ public class Music : MonoBehaviour
         else
         {
             DontDestroyOnLoad(gameObject);
+        }
+        song = soundTrack[SceneManager.GetActiveScene().buildIndex];
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(song);
         }
     }
 }
